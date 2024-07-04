c16bceb8b8bb0cadb543cd97e16443ca7ca79736
Serge Mosesov <smosesov@gmail.com>
2024-07-04 14:20:46 +0100

    fix: add Chunk to init

--- src/onecontext/__init__.py
+++ src/onecontext/__init__.py
@@ -1,4 +1,5 @@
-from onecontext.main import OneContext
 from onecontext.__about__ import __version__
+from onecontext.main import OneContext
+from onecontext.pipeline import Chunk
 
-__all__ = ["OneContext", "__version__"]
+__all__ = ["OneContext", "__version__", "Chunk"]
