3b9b6447ba07801bc3bb4c450b15251f17691101
Your Name <smosesov@gmail.com>
2024-09-17 19:50:04 +0100

    v2 tests pass

--- tests/test_search_context.py
+++ tests/test_search_context.py
@@ -26,35 +26,6 @@ def context_with_files(context: Context, file_paths: list):
     assert len(files) > 0
 
 
-def test_search_chunks(context_with_files: Context):
-    chunks = context_with_files.search("sample query", top_k=10)
-    all_meta = {chunk.metadata_json for chunk in chunks}
-
-    breakpoint()
+def test_query(context_with_files: Context):
+    chunks = context_with_files.query("sample query", top_k=10)
     assert len(chunks) == 10
-    chunks = context_with_files.search("sample query", top_k=10, metadata_filters={"file_tag": {"$eq": "file_1"}})
-
-    contrained_meta = {chunk.metadata for chunk in chunks}
-
-    breakpoint()
-    assert len(chunks) == 10
-
-
-def test_list_chunks(context_with_files: Context):
-    files = context_with_files.list_files()
-
-    file_ids = [file.id for file in files]
-
-    chunks_no_filters = context_with_files.list_chunks(limit=100)
-    assert all(chunk.file_id in file_ids for chunk in chunks_no_filters)
-
-    specific_chunks_list = context_with_files.list_chunks(file_id=file_ids[0], limit=100)
-    assert all(chunk.file_id == file_ids[0] for chunk in specific_chunks_list)
-
-    metadata_filters = {"file_tag": {"$eq": "file_1"}}
-
-    chunks_metadata_filtered = context_with_files.list_chunks(metadata_filters=metadata_filters, limit=100)
-
-    for chunk in chunks_metadata_filtered:
-        if chunk.metadata_json and not chunk.metadata_json.get("file_tag") == "file_1":
-            assert False, "Chunk filtered by metadata does not contain correct metadata tag."
--- tests/test_upload_files.py
+++ tests/test_upload_files.py
@@ -19,36 +19,8 @@ def context(client: OneContext):
     client.delete_context(context_name)
 
 
-def download_file(file_url, local_path):
-    response = requests.get(file_url)
-    if response.status_code == 200:
-        with open(local_path, "wb") as f:
-            f.write(response.content)
-
-
-def test_upload_files(client: OneContext, context: Context, file_paths: list):
-    metadata = [{"file_tag": "file_1"}, {"file_tag": "file_2"}]
-    context.upload_files(file_paths, metadata=metadata)
+def test_upload_files(context: Context, file_paths: list):
+    context.upload_files(file_paths)
     wait_for_file_processing(context)
-
     files = context.list_files()
-    file_names_and_meta = {os.path.basename(file_path): meta for file_path, meta in zip(file_paths, metadata)}
-
-    for file in files:
-        assert file_names_and_meta[file.name] == file.metadata_json
-
     assert len(files) == 2
-
-    file_map = {f.name: f for f in files}
-    for original_path in file_paths:
-        local_filename = os.path.basename(original_path)
-        file_to_download = file_map[local_filename]
-
-        with tempfile.NamedTemporaryFile() as temp_file:
-            download_file(context.get_download_url(file_to_download.id), temp_file.name)
-
-            with open(original_path, "rb") as original:
-                temp_file.seek(0)
-                assert (
-                    original.read() == temp_file.read()
-                ), f"The downloaded file content for {local_filename} differs from the original."
