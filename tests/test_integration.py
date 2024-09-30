4af39cfce300777389fc116985c606fb13b49d26
Your Name <smosesov@gmail.com>
2024-09-30 15:20:14 +0100

    fix: update test for  more directory files

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -21,6 +21,6 @@ def test_integration(context: Context, test_files_directory: str):
     context.upload_from_directory(test_files_directory)
     wait_for_file_processing(context)
     files = context.list_files()
-    assert len(files) == 2
+    assert len(files) == 3
     chunks = context.search("sample query", top_k=10)
     assert len(chunks) == 10
